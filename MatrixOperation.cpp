e5379781ad74ec628c673a91f98c3d872dcac2a5
yilehu <yilehu@email.arizona.edu>
2019-12-09 13:55:20 +0800

    Matrix is stored in banded format.

--- MatrixOperation.cpp
+++ MatrixOperation.cpp
@@ -20,6 +20,27 @@ void MatrixDefinition(double **Matrix,int m,int Bandwidth)
 	}
 }
 
+void MatrixDefinition_Banded(double **Matrix,int m,int Bandwidth)
+{
+	int i,j,k;
+	for(i=0;i<m;i++)
+	{
+	
+		for(j=0;j<Bandwidth;j++)
+		{
+			if(i>Bandwidth-j-2)
+			{
+				Matrix[i][j] = j+1;
+				
+			}
+			if(i<m-(Bandwidth-1-j))
+			{
+				Matrix[i][2*Bandwidth-2 - j] = j+1;
+			}
+		}
+	}
+}
+
 void MatrixMultiply(double **A,double *x,double *b,int m,int n)
 {
 	int i,j;
@@ -29,6 +50,35 @@ void MatrixMultiply(double **A,double *x,double *b,int m,int n)
 		for(j=0;j<n;j++)
 		{
 			b[i] += A[i][j]*x[j];
+		}
+	}
+}
+
+void MatrixMultiply_Banded(double **A,double *x,double *b,int m,int n,int Bandwidth)
+{
+	int i,j;
+	for(i=0;i<Bandwidth-1;i++)
+	{
+		b[i] = 0.0;
+		for(j=Bandwidth-1-i;j<n;j++)
+		{
+			b[i] += A[i][j]*x[i-(Bandwidth-1)+j];
+		}
+	}
+	for(i=Bandwidth-1;i<m-Bandwidth+1;i++)
+	{
+		b[i] = 0.0;
+		for(j=0;j<n;j++)
+		{
+			b[i] += A[i][j]*x[i-(Bandwidth-1)+j];
+		}
+	}
+	for(i=m-Bandwidth+1;i<m;i++)
+	{
+		b[i] = 0.0;
+		for(j=0;j<Bandwidth-1+m-i;j++)
+		{
+			b[i] += A[i][j]*x[i-(Bandwidth-1)+j];
 		}
 	}
 }
