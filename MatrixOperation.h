4997e3080723f0a886409c91ce98c1f07c6f7a2e
yilehu <yilehu@email.arizona.edu>
2019-12-13 16:06:35 +0800

    Debug

--- MatrixOperation.h
+++ MatrixOperation.h
@@ -3,7 +3,11 @@
 
 void MatrixDefinition(double **Matrix,int m,int Bandwidth);
 
+void MatrixDefinition_Banded(double **Matrix,int m,int Bandwidth);
+
 void MatrixMultiply(double **A,double *x,double *b,int m,int n);
+
+void MatrixMultiply_Banded(double **A,double *x,double *b,int m,int n,int Bandwidth);
 
 double Dotproduct(double *a,double *b,int n);
 
